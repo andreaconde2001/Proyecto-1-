df1e41863542811a370b0a4f25f18a450fb78b65
andreaconde2001 <96255697+andreaconde2001@users.noreply.github.com>
2022-01-27 21:00:18 -0500

    Update cargo.py

--- cargo.py
+++ cargo.py
@@ -1,3 +1,8 @@
+#Nombre: Andrea Conde Pérez
+#Materia: Estructura de datos
+#Carrera: Ingeniería en software 3er nivel Aula - C1
+
+
 class Cargo:
   secuencia=2
   cargos = [{"codigo":1,"cargo":"Analista"}, {"codigo":2,"cargo":"Jefe"}]
